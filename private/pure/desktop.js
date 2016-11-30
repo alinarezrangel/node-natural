53e2c6e1053822e98107b95acb9397576ac70cf8
Alejandro Linarez Rangel <alinarezrangel@gmail.com>
2016-11-30 11:33:58 -0400

    Working window example

--- private/pure/desktop.js
+++ private/pure/desktop.js
@@ -22,41 +22,233 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ***************************************************************************/
 
+var PureWindows = [];
+var PureMaxZIndex = 2;
+
 function PureMakeTextNode(text)
 {
 	return document.createTextNode(text);
 }
 
-function PureExecuteTemplate(template, arguments)
+function PureEmitEvent(target, name, data)
 {
-	var tmp = document.importNode(template, true);
+	var ev = new CustomEvent(name, data);
+	return target.dispatchEvent(ev);
+}
+
+function PureEmitEvents(targets, name, data)
+{
+	var i = 0;
+	var j = targets.length;
+	for(i = 0; i < j; i++)
+	{
+		PureEmitEvent(targets[i], name, data);
+	}
+}
+
+function PureExecuteTemplate(template, args)
+{
+	var tmp = document.importNode(template.content, true);
 	var t = $(tmp);
 
 	var classes = t.find("*[data-template-class-insert]");
-	classes.each(function(node)
+	classes.each(function(index)
 	{
-		node.addClass(arguments[node.data("templateClassInsert")]);
+		var node = $(this);
+		node.addClass(args[node.data("templateClassInsert")]);
 	});
 	var texts = t.find("*[data-template-text-insert]");
-	texts.each(function(node)
+	texts.each(function(index)
 	{
-		node.get(0).appendChild(PureMakeTextNode(arguments[node.data("templateTextInsert")]));
+		var node = $(this);
+		node.get(0).appendChild(PureMakeTextNode(args[node.data("templateTextInsert")]));
 	});
 
-	return tmp;
+	return t.children().get(0);
+}
+
+function PureMakeDefaultWindowLayout(args)
+{
+	var win = PureExecuteTemplate(
+		$("#puredesktop-window").get(0),
+		args
+	);
+	var titlebar = $(win).find(".puredesktop-window-titlebar").get(0);
+	win.style.width = "300px";
+	win.style.height = "300px";
+	win.style.zIndex = 1;
+
+	$(win).data("clickX", 0);
+	$(win).data("clickY", 0);
+	$(win).data("mousedown", "false");
+	$(win).data("movable", "false");
+	$(win).data("focused", "false");
+	$(win).data("instantRemove", "true");
+	$(win).data("preventTitlebarMove", "false");
+
+	win.addEventListener("exit", function(ev)
+	{
+		if($(this).data("instantRemove") == "true")
+		{
+			PureMaxZIndex -= 1;
+			PureWindows.splice(parseInt($(this).data("pureIndex")), 1);
+			$(this).remove();
+		}
+	});
+
+	win.addEventListener("opened", function(ev)
+	{
+		PureMaxZIndex += 1;
+	});
+
+	win.addEventListener("hide", function(ev)
+	{
+		$(this).addClass("hidden");
+	});
+
+	win.addEventListener("show", function(ev)
+	{
+		$(this).removeClass("hidden");
+	});
+
+	win.addEventListener("windowPriorityChanged", function(ev)
+	{
+		var iz = parseInt(this.style.zIndex);
+		if(iz > 1)
+			this.style.zIndex = (iz - 1) + "";
+		$(this).data("focused", "false");
+	});
+
+	win.addEventListener("focus", function(ev)
+	{
+		if($(this).data("focused") == "false")
+		{
+			PureEmitEvents(PureWindows, "windowPriorityChanged", {
+				"from": this
+			});
+			this.style.zIndex = PureMaxZIndex;
+			$(this).data("focused", "true");
+		}
+	});
+
+	win.addEventListener("mousedown", function(ev)
+	{
+		PureEmitEvent(this, "focus", {});
+		if($(this).data("movable") == "true")
+		{
+			$(this).data("mousedown", "true");
+			$(this).data("clickX", (ev.clientX - $(this).offset().left));
+			$(this).data("clickY", (ev.clientY - $(this).offset().top));
+			ev.preventDefault();
+			return false;
+		}
+	});
+
+	win.addEventListener("mouseup", function(ev)
+	{
+		if($(this).data("movable") == "true")
+		{
+			$(this).data("mousedown", "false");
+			this.style.cursor = "auto";
+			ev.preventDefault();
+			return false;
+		}
+	});
+
+	win.addEventListener("mousemove", function(ev)
+	{
+		if(($(this).data("mousedown") == "true") && ($(this).data("movable") == "true"))
+		{
+			var left = $(this).data("clickX");
+			var top = $(this).data("clickY");
+			var topnavtp = $(".puredesktop-top-menubar").height() + 2;
+			var leftnavtp = $(".puredesktop-left-menubar").width() + 2;
+			$(this).data("defaultTop", Math.max(ev.clientY - top, topnavtp));
+			$(this).data("defaultLeft", Math.max(ev.clientX - left, leftnavtp));
+			this.style.top = Math.max(ev.clientY - top, topnavtp) + "px";
+			this.style.left = Math.max(ev.clientX - left, leftnavtp) + "px";
+			this.style.cursor = "move";
+			ev.preventDefault();
+			return false;
+		}
+	});
+
+	win.addEventListener("click", function(ev)
+	{
+		if($(this).data("focused") == "false")
+		{
+			PureEmitEvent(this, "focus", {});
+			ev.preventDefault();
+			return false;
+		}
+	});
+
+	titlebar.addEventListener("mousedown", function()
+	{
+		if($(win).data("preventTitlebarMove") == "true")
+			return;
+		$(win).data("movable", "true");
+		$(win).data("mousedown", "true");
+		$(win).removeClass("puredesktop-can-select");
+	});
+
+	titlebar.addEventListener("mouseup", function()
+	{
+		if($(win).data("preventTitlebarMove") == "true")
+			return;
+		$(win).data("movable", "false");
+		$(win).data("mousedown", "false");
+		$(win).addClass("puredesktop-can-select");
+		win.style.cursor = "auto";
+	});
+
+	$(titlebar).find(".puredesktop-btn-exit").get(0).addEventListener("click", function()
+	{
+		PureEmitEvent(win, "exit", {
+			"edata": "window.buttons.exit:clicked"
+		});
+	});
+
+	$(titlebar).find(".puredesktop-btn-move").get(0).addEventListener("mousedown", function(ev)
+	{
+		if($(win).data("movable") == "false")
+		{
+			$(this).addClass("color-light-grey");
+			$(win).data("movable", "true");
+			$(win).data("preventTitlebarMove", "true");
+			$(win).removeClass("puredesktop-can-select");
+			PureEmitEvent(win, "stackeable", {});
+		}
+		else
+		{
+			$(this).removeClass("color-light-grey");
+			$(win).data("movable", "false");
+			$(win).data("preventTitlebarMove", "false");
+			$(win).addClass("puredesktop-can-select");
+			PureEmitEvent(win, "fixeable", {});
+		}
+	});
+
+	var li = PureWindows.push(win) - 1;
+
+	$(win).data("pureIndex", li);
+
+	return win;
 }
 
 NaturalOnLoadevent = function()
 {
-	$(".puredesktop-main-content").get(0).appendChild(
-		PureExecuteTemplate(
-			$("#puredesktop-window").get(0),
-			{
-				"color": "color-natural-magenta",
-				"title": "Hello World"
-			}
-		)
-	);
+	var win = PureMakeDefaultWindowLayout({
+		"color": "color-natural-indigo",
+		"title": "Hello World",
+		"bkgcolor": "color-natural-white"
+	});
+	PureEmitEvent(win, "opened", {});
+	var ct = $(win).find(".puredesktop-window-content").get(0);
+
+	ct.appendChild(PureMakeTextNode("Hello World"));
+
+	$(".puredesktop-main-content").get(0).appendChild(win);
 };
 
 window.addEventListener("load", function()
